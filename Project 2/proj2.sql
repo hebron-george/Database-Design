4f8ad63eaf5b3a47c662c49f3ac0f1c67f339837
Hebron George <hebrontgeorge@gmail.com>
2012-10-16 11:35:23 -0400

    Moving everything

---
+++ Project 2/proj2.sql
@@ -0,0 +1,57 @@
+REM -------------------------------------------
+REM COP4710 Fall 2012 
+REM Project 2 - NBA Databse
+REM Your name: Hebron George
+REM Your NetID: hgeorge3
+REM -------------------------------------------
+
+REM Do NOT delete the following two lines! 
+REM Change output file name to proj2-xxx.out where xxx is your NetID
+
+SET PAGESIZE 500;
+SPOOL proj2.out;
+
+
+REM Problem 1
+REM SELECT system.PLAYERREGULARSEASON.FIRSTNAME, system.PLAYERREGULARSEASON.LASTNAME, system.PLAYERREGULARSEASON.TEAM, system.PLAYERREGULARSEASON.YEAR FROM system.PLAYERREGULARSEASON WHERE system.PLAYERREGULARSEASON.TEAM = 'MIA';
+REM SELECT system.PLAYERREGULARSEASON.FIRSTNAME, system.PLAYERREGULARSEASON.LASTNAME FROM system.PLAYERREGULARSEASON WHERE system.PLAYERREGULARSEASON.TEAM = 'NYK') EXCEPT (SELECT system.PLAYERREGULARSEASON.FIRSTNAME, system.PLAYERREGULARSEASON.LASTNAME WHERE system.PLAYERREGULARSEASON.TEAM = 'MIA');
+
+
+REM Problem 2
+
+
+REM Problem 3
+
+
+REM Problem 4
+
+
+REM Problem 5
+
+
+REM Problem 6
+
+
+REM Problem 7
+
+
+REM Problem 8
+
+
+REM Problem 9
+
+
+REM Problem 10
+
+
+REM Problem 11
+
+
+REM Problem 12
+
+
+REM Do NOT change the following line
+SPOOL OFF;
+
+
+
